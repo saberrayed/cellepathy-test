f2d5639af8116a84a73c2660fce76bd8d214174c
Rey Basadre <reyb@codev.com>
2019-10-01 08:38:14 +0800

    Added TTS

--- app/src/main/java/com/codev/exam/AudioClass.java
+++ app/src/main/java/com/codev/exam/AudioClass.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
 
 public class AudioClass
 {
+    private TextToSpeech tts;
     private AudioManager audioManager;
     private Activity activity;
     private int maxVolume;
@@ -20,6 +25,25 @@ public class AudioClass
         maxVolume = audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC);
         audioManager.setStreamVolume(audioManager.STREAM_MUSIC, maxVolume, 1);
         audioManager.setSpeakerphoneOn(true);
+
+        tts = new TextToSpeech(activity, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if (status == TextToSpeech.SUCCESS) {
+                    int result=tts.setLanguage(Locale.US);
+
+                    if (result ==TextToSpeech.LANG_MISSING_DATA ||
+                        result== TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    } else {
+                        // playTTS();
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
     }
 
     public void playAudio() {
@@ -27,5 +51,21 @@ public class AudioClass
         mp.setAudioStreamType(AudioManager.MODE_IN_COMMUNICATION);
         mp.setLooping(true);
         mp.start();
+    }
+
+    public void shutdownTTS() {
+        if (tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+    }
+
+    public void playTTS(String speech) {
+        if (speech == null|| "".equals(speech)) {
+            speech = "Content not available";
+            tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        } else {
+            tts.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 }
--- app/src/main/java/com/codev/exam/MainActivity.java
+++ app/src/main/java/com/codev/exam/MainActivity.java
@@ -9,34 +9,42 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private TextView txtMessage;
+    private EditText edtSpeech;
+    private AudioClass audio;
+
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        String action = intent.getAction();
+        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                txtMessage.setText("Device found!");
-            }
-            else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-                txtMessage.setText("Device is now connected.");
-            }
-            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                txtMessage.setText("Done searching device.");
-            }
-            else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
-                txtMessage.setText("Device is about to disconnect...");
-            }
-            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-                txtMessage.setText("Device is disconnected.");
-            }
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            txtMessage.setText("Device found!");
+        }
+        else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+            txtMessage.setText("Device is now connected.");
+        }
+        else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+            txtMessage.setText("Done searching device.");
+        }
+        else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+            txtMessage.setText("Device is about to disconnect...");
+        }
+        else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+            txtMessage.setText("Device is disconnected.");
+        }
         }
     };
 
@@ -46,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
         txtMessage = findViewById(R.id.txtMessage);
+        edtSpeech = findViewById(R.id.edtSpeech);
+        audio = new AudioClass(MainActivity.this);
 
         if (!isBluetoothHeadsetConnected()) {
             txtMessage.setText("Waiting for device.");
@@ -62,9 +72,12 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnPlay).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AudioClass audio = new AudioClass(MainActivity.this);
-                audio.playAudio();
-                txtMessage.setText("Now playing audio..\nTo restart program, please close the app.");
+            // audio.playAudio();
+            // txtMessage.setText("Now playing audio..\nTo restart program, please close the app.");
+
+            String speech = edtSpeech.getText().toString();
+            audio.playTTS(speech);
+            txtMessage.setText("Now playing '" + speech + "'");
             }
         });
     }
@@ -73,5 +86,11 @@ public class MainActivity extends AppCompatActivity {
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         return mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()
                 && mBluetoothAdapter.getProfileConnectionState(BluetoothHeadset.HEADSET) == BluetoothHeadset.STATE_CONNECTED;
+    }
+
+    @Override
+    protected void onPause() {
+        audio.shutdownTTS();
+        super.onPause();
     }
 }
